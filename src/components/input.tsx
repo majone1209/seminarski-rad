9a66c4d12232627c5cd7623cad0a2d387c0e0d47
majone1209 <maja.juratovac12@gmail.com>
2023-06-29 16:04:00 +0200

    sem zadnje 2906

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,32 +1,20 @@
-import { InputHTMLAttributes } from "react";
-import { useState, ChangeEvent, FormEvent } from "react";
-
-//InputProps je tip koji definira propertyje koje Input komponenta očekuje. Ovaj tip proširuje InputHTMLAttributes<HTMLInputElement> kako bi uključio standardne HTML atribute za input element. (nisam sigurna treba li se to primijeniti)
-
+import  { useState, ChangeEvent, FormEvent, InputHTMLAttributes } from "react";
 type InputProps = {
   onSendMessage: (message: string) => void;
   text: string;
- } & InputHTMLAttributes<HTMLInputElement>;
-
-//Input komponenta prima onSendMessage kao property. Unutar komponente, koristi se useState kako bi se pratilo stanje teksta unesenog u input polje. 
-
-const Input = ({ onSendMessage }: InputProps) => {
-  const [text, setText] = useState<string>("");
-
-//handleChange funkcija se poziva pri promjeni sadržaja input polja. Prima ChangeEvent objekt i ažurira stanje teksta na novu vrijednost unesenu u polje.  
-
-const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-  setText(e.target.value);
-};
-
-//Funkcija se poziva prilikom slanja obrasca (klikom na gumb "Send"). Sprječava se refreshanje stranice pozivom e.preventDefault(). Ako je tekst poruke (text.trim()) različit od praznog stringa, poziva se onSendMessage funkcija s tekstom poruke kao argumentom, a stanje teksta se postavlja na prazan string kako bi se očistilo input polje.
-
-const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  onSendMessage(text);
-  setText("");
-
-//Ova funkcija vraća JSX; definiran je obrazac s inputom i pripadajućim atributima.  
+} & InputHTMLAttributes<HTMLInputElement>;
+const Input = ({ onSendMessage }:InputProps) => {
+  const [text, setText] = useState("");
+  const onChange = (e: ChangeEvent) => {
+    setText(e.target.value);
+  };
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (text.trim() !== "") {
+      setText("");
+      onSendMessage(text);
+    }
+  };
   return (
     <div className="Input">
       <form onSubmit={onSubmit}>
@@ -34,14 +22,12 @@ const onSubmit = (e: FormEvent<HTMLFormElement>) => {
           onChange={onChange}
           value={text}
           type="text"
-          placeholder="Enter your message and press ENTER"
-          autoFocus={true}
+          placeholder="Upiši poruku i stisni ENTER"
+          autoFocus
         />
-        <button>Send</button>
+        <button type="submit">Pošalji</button>
       </form>
     </div>
   );
 };
-}
-
 export default Input;
--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -1,61 +1,38 @@
-//Nvedena tri sučelja opisuju strukturu objekta, u ovom slučaju člana (bit će prikazan u poruci) koji ima određene propertyje.
-
+import React, { useState } from "react";
 interface Member {
   [x: string]: any;
-    color: string;
-    username: string;
-    id: string; 
+  username: string;
+  color: string;
+  id: string;
 }
-
-interface Message {
+type Message = {
   member: Member;
-  text: any;
-  id: any;
-}
-
-interface MessagesProps {
+  text: string;
+};
+type MessagesProps = {
   messages: Message[];
   currentMember: Member;
-}
-
-//komponenta Messages prima messages i currentMember kao propse.
-
-const Messages = ({ messages, currentMember }: MessagesProps): JSX.Element => {
-
-//renderMessage funkcija se koristi za prikazivanje pojedinačne poruke. Provjeravamo je li poruka poslana od trenutnog člana(messageFromMe) ili nije. Ukoliko su ova dva identifikatora jednaka, to znači da je poruka poslana od trenutnog člana, a vrijednost varijable messageFromMe će biti true. Ako nisu jednaki, to znači da poruka nije poslana od trenutnog člana, te će vrijednost varijable messageFromMe biti false.
-const renderMessage = (message: Message) => {
-  const { member, text } = message;
-  const messageFromMe = member.id === currentMember.id;
-
-//Gore navedena provjera koristi se kako bi se odredilo koju klasu (className) dodijeliti poruci prilikom prikazivanja. Ako je poruka poslana od trenutnog člana, koristit će se klasa "Messages-message currentMember", a ako nije, koristit će se samo "Messages-message".
-  const className = messageFromMe
-    ? "Messages-message currentMember"
-    : "Messages-message";
-
-//Ova funkcija vraća JSX koji predstavlja pojedinačnu poruku. Svjostvo className ovisi o tome je li je poruka poslana od trenutnog člana ili ne; div element s klasom "Message-content" sadrži unutarnje elemente koji prikazuju korisničko ime (member.username) i tekst poruke (text).
-  return (
-    <li className={className}>
-      <span
-        className="avatar"
-        style={{ backgroundColor: member.color }}
-      />
-      <div className="Message-content">
-        <div className="username">{member.username}</div>
-        <div className="text">{text}</div>
-      </div>
-    </li>
-  );
-}; 
-
-//Messages komponenta vraća JSX koji predstavlja listu poruka. Koristimo metodu mapiranja. Svaka poruka ima jedinstveni key atribut koji je postavljen na message.id. Na taj način ažuriramo listu poruka. Pozivamo renderMessage funckiju kako bi se prikazala pojedinačna poruka.
-return (
-    <ul className="Messages-list">
-      {messages.map((message) => (
-        <li key={message.id}> {renderMessage(message)}
-        </li>
-      ))}
-    </ul>
-  );
 };
-
-export default Messages; // komponentu Messages importat ćemo i koristiti u App.tsx komponenti.
+const Messages: React.FC<MessagesProps> = ({ messages, currentMember }) => {
+  const renderMessage = (message: Message) => {
+    const { member, text } = message;
+    const messageFromMe = currentMember.id;
+    const className = messageFromMe
+      ? "Messages-message currentMember"
+      : "Messages-message";
+    return (
+      <li className={className}>
+        <span
+          className="avatar"
+          style={{ backgroundColor: currentMember.color }}
+        />
+        <div className="Message-content">
+          <div className="username">{currentMember.username}</div>
+          <div className="text">{text}</div>
+        </div>
+      </li>
+    );
+  };
+  return <ul className="Messages-list">{messages.map(renderMessage)}</ul>;
+};
+export default Messages;
